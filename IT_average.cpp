6fffbf27a7129e94ad3717fe84bdba8976fcb543
Naveen Kumar sahu <84307098+Naveens24@users.noreply.github.com>
2022-07-29 09:14:27 +0530

    Create IT_average.cpp

---
+++ IT_average.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, c, d, e;
+    double avg;
+    cout<<"Programme to print the average of two number"<<endl;
+    cout<<"Enter the 5 number to print average of it:"<<endl;
+    cin>>a>>b>>c>>d>>e;
+   
+    avg = (a+b+c+d+e)/5;
+    cout <<endl<< "The average of 5 number is: "<< avg<<endl;
+
+
+}
