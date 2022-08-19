45a147b4ed25e7bab3bd69ddcde9d9a95b7020d1
Naveen Kumar sahu <84307098+Naveens24@users.noreply.github.com>
2022-08-19 16:38:59 +0530

    Create IT_PostfixToPrefix.cpp

---
+++ IT_PostfixToPrefix.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+bool isOperand(char c)
+{
+    if((c>='a' && c<='z' ) || (c>='A' && c<='Z'))
+    {
+        return true;
+    }
+
+    else
+    return false;
+}
+
+string PostfixToPrefix(string postfix)
+{
+    stack<string> s;
+
+    for(int i=0; i<postfix.length(); i++)
+    {
+        if(isOperand(postfix[i]))
+        {
+            // Converting the char data type in the string
+            string op(1, postfix[i]); 
+            s.push(op);
+        }
+        else
+        {
+            string op1= s.top();
+            s.pop();
+            string op2 = s.top();
+            s.pop();
+            s.push(postfix[i]+op2+op1);
+        }
+
+    }
+    return s.top();
+
+}
+
+int main(){
+    string postfix, prefix;
+    cout<<"Enter the Postfix Expression to conver it into prefix: ";
+    cin>>postfix;
+    cout<<endl;
+    cout<<"POSTFIX EXPRESSION IS: "<<postfix<<endl;
+    prefix = PostfixToPrefix(postfix);
+    cout<<"PREFIX EXPRESSION IS: "<<prefix;
+
+    
+return 0;
+}
